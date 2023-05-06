fcea3a10ee3a764d6ca577f38f0743566cb3175a
lstidam2 <lstidam2@ivytech.edu>
2023-05-05 23:05:09 -0400

    added registration to login page

--- register/forms.py
+++ register/forms.py
@@ -9,6 +9,6 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["email", "username", "password1", "password2"]
         
 
